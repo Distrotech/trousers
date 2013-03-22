801796399cbe481af8774fa935810f617c15b94e
Kent Yoder <key@linux.vnet.ibm.com>
2013-03-22 10:53:43 -0500

    Return success in owner_evict_init() in the case where the last key we query for status returns TPM_E_INVALID_KEYHANDLE.
    Reported-by: Jan Just Keijser <janjust@nikhef.nl>
    Signed-off-by: Kent Yoder <key@linux.vnet.ibm.com>

--- src/tcs/tcs_key_mem_cache.c
+++ src/tcs/tcs_key_mem_cache.c
@@ -1099,8 +1099,11 @@ owner_evict_init()
 							  sizeof(UINT32), (BYTE *)&keyHandle,
 							  &respDataSize, &respData);
 		/* special case, invalid keys are automatically evicted later */
-		if (result == TPM_E_INVALID_KEYHANDLE)
+		if (result == TPM_E_INVALID_KEYHANDLE) {
+			result = TSS_SUCCESS;
 			continue;
+		}
+
 		if (result != TSS_SUCCESS) {
 			free(keyList.handle);
 			return result;
