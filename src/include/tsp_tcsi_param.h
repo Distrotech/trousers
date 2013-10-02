982c3ebe9d9a00284650a8745d3e8fb912f3d307
Richard Maciel <rmaciel@linux.vnet.ibm.com>
2013-10-02 16:56:22 -0300

    Add functions to fetch hostname and port info to clients
    Modules included will be used by the next modifications to
    retrieve hostname and port information to setup client
    connections to the tcsd.
    
    Signed-off-by: Richard Maciel <rmaciel@linux.vnet.ibm.com>
    Acked-by: Joel Schopp <jschopp@linux.vnet.ibm.com>

---
+++ src/include/tsp_tcsi_param.h
@@ -0,0 +1,19 @@
+
+/*
+ * Licensed Materials - Property of IBM
+ *
+ * trousers - An open source TCG Software Stack
+ *
+ * (C) Copyright International Business Machines Corp. 2005, 2007, 2013
+ *
+ */
+
+/* Defines which environment var is responsible for setting the port to
+ * which the client will connect */
+#define PORT_ENV_VAR "TSS_TCSD_PORT"
+
+/* Defines which envionment var is responsible for setting the hostname to
+ * which the client will connect */
+#define HOSTNAME_ENV_VAR "TSS_TCSD_HOSTNAME"
+
+#define TCP_PORT_STR_MAX_LEN 6
